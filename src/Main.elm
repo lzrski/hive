fb41659e93da1a19a81a6efd0def709fc7326d09
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-23 19:07:17 +0000

    Transition to a program with time susbsciption

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,51 @@
 module Main exposing (..)
 
-import Html exposing (Html, h1, text)
+import Html exposing (Html, program, text, h1, div, pre)
+import Time exposing (Time, second)
 
 
-main : Html
+type alias Model =
+    Time
+
+
 main =
-    h1 [] [ text "Hello!" ]
+    program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 []
+            [ text "Hello!" ]
+        , pre
+            []
+            [ text <| toString model ]
+        ]
+
+
+type Msg
+    = NoOp
+    | Tick Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick now ->
+            ( now, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+init =
+    ( 0, Cmd.none )
+
+
+subscriptions model =
+    Time.every second Tick
