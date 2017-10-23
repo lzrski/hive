de627bce96d72d23082799918da2e7af39c6aef8
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-23 19:13:27 +0200

    Fix Elm installation on NixOS
    Elm can't be installed via NPM on NixOS:
    
        Error: spawn
    ./node_modules/elm/Elm-Platform/0.18.0/.cabal-sandbox/bin/elm ENOENT
    
    Better to install it via Nix.
    
    Also add elm-format.

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
 with import <nixpkgs> {}; stdenv.mkDerivation {
   name = "node";
-  buildInputs = [ nodejs-8_x ];
+  buildInputs = [
+    nodejs-8_x
+    elmPackages.elm
+    elmPackages.elm-format
+  ];
   shellHook = ''
     # Start user's preffered shell
     exec ${builtins.getEnv "SHELL"}
-    '';
+  '';
 }
