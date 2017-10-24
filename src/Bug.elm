d98b449569a26b0066c0f9bfd16ee0af08d778ea
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-24 14:26:45 +0000

    Let the hungry bugs go out and fed ones in

--- src/Bug.elm
+++ src/Bug.elm
@@ -1,6 +1,8 @@
 module Bug exposing (Bug, create, update)
 
+import OpenSolid.Direction2d exposing (Direction2d)
 import OpenSolid.Point2d exposing (Point2d)
+import OpenSolid.Vector2d
 import Time exposing (Time)
 
 
@@ -17,4 +19,29 @@ create point =
 
 update : Time -> Bug -> Bug
 update delta bug =
-    { bug | nutrition = bug.nutrition - (delta / 10000) }
+    let
+        towardOrigin =
+            OpenSolid.Direction2d.from bug.position OpenSolid.Point2d.origin
+
+        {- If bug is hungry, it will go out to seek food.
+           If it's fed, it will go toward the origin.
+        -}
+        direction =
+            if bug.nutrition > 0.7 then
+                towardOrigin
+            else
+                Maybe.map OpenSolid.Direction2d.flip towardOrigin
+    in
+        case direction of
+            Just dir ->
+                let
+                    velocity =
+                        OpenSolid.Vector2d.with { length = 0.1, direction = dir }
+                in
+                    { bug
+                        | nutrition = bug.nutrition - (delta / 10000)
+                        , position = OpenSolid.Point2d.translateBy velocity bug.position
+                    }
+
+            Nothing ->
+                bug
