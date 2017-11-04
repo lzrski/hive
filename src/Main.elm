855b7798aefb39f858e4ecc3f94a0c2ac91d85da
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-11-04 00:36:33 +0000

    Implement basic reason -> perform world update
    Introduce a new type: World.
    
    World consists of Entities (Dict Id Entity) and a seed, that will be
    used to assign Ids to new entities (so they can be uniquely identified).
    
    On every frame world is updated. Entities are mapped to Actions. An
    Action represents the intention of an entity. Then actions are
    performed, resulting in a new World.
    
    The logic to calculate actuall actions and to perform them is not yet
    implemented (every action is Idle and the world is static).
    
    Whole code was put back into one file. Modularization seems premature.

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,161 @@
-module Main exposing (..)
+module Main exposing (main)
 
+import Time exposing (Time, second)
+import Dict exposing (Dict)
 import AnimationFrame
-import Bug exposing (Bug)
-import Food exposing (Food)
+
+
+-- Geometry
+
+import OpenSolid.BoundingBox2d as BoundingBox2d
+import OpenSolid.Point2d as Point2d exposing (Point2d)
+import OpenSolid.Direction2d as Direction2d exposing (Direction2d)
+import OpenSolid.Vector2d as Vector2d exposing (Vector2d)
+
+
+-- View related
+
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import OpenSolid.Point2d as Point2d exposing (Point2d)
-import OpenSolid.BoundingBox2d as BoundingBox2d
 import Svg exposing (Svg)
-import Svg.Attributes as Attributes
-import OpenSolid.Svg as Svg
 import Svg.Attributes exposing (..)
-import Time exposing (Time, second)
+import OpenSolid.Svg as Svg
 
 
 type alias Model =
     { elapsed : Time
     , paused : Bool
-    , entities : List Entity
+    , world : World
     }
 
 
+type alias World =
+    { seed : ID
+    , entities : Entities
+    }
+
+
+type alias ID =
+    Int
+
+
+type alias Entities =
+    Dict ID Entity
+
+
 type Entity
-    = Bug Bug
-    | Food Food
+    = Bug
+        { position : Point2d
+        , nutrition : Float
+        }
+    | Food
+        { position : Point2d
+        , quantity : Float
+        }
+
+
+type Action
+    = Idle
+    | Crawl Direction2d
+    | Consume ID
+
+
+type alias Actions =
+    Dict ID Action
+
+
+
+-- Public constructors
+
+
+world_empty : World
+world_empty =
+    { seed = 0
+    , entities = Dict.empty
+    }
+
+
+world_insert : Entity -> World -> World
+world_insert entity { seed, entities } =
+    { seed = seed + 1
+    , entities = Dict.insert seed entity entities
+    }
+
+
+bug : ( Float, Float ) -> Entity
+bug coordinates =
+    Bug
+        { position = (Point2d.fromCoordinates coordinates)
+        , nutrition = 1.0
+        }
+
+
+food : ( Float, Float ) -> Entity
+food coordinates =
+    Food
+        { position = (Point2d.fromCoordinates coordinates)
+        , quantity = 1.0
+        }
+
+
+
+-- Update
+
+
+world_update : Time -> World -> World
+world_update delta world =
+    world.entities
+        |> reason
+        |> \actions -> perform delta actions world
+
+
+reason : Entities -> Actions
+reason =
+    Dict.map
+        (\key entity ->
+            let
+                _ =
+                    Debug.log "Reasoning" entity
+            in
+                Idle
+        )
+
+
+perform : Time -> Actions -> World -> World
+perform delta actions world =
+    {- TODO: Fold actions into a new world, starting with current -}
+    actions
+        |> Dict.foldl
+            (\id action world ->
+                let
+                    _ =
+                        Debug.log "Performing action" action
+
+                    { entities, seed } =
+                        world
+
+                    entity =
+                        Dict.get id entities
+                in
+                    case entity of
+                        -- Entity was previously removed. Move on.
+                        Nothing ->
+                            world
+
+                        -- TODO: Perform the action of a bug
+                        Just (Bug bug) ->
+                            case action of
+                                Idle ->
+                                    world
+
+                                _ ->
+                                    world
+
+                        -- Food can take no actions ATM
+                        Just (Food food) ->
+                            world
+            )
+            world
 
 
 main =
@@ -38,26 +170,22 @@ main =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 []
-            [ Html.text "Hello!" ]
-        , pre
+        [ code
             []
             [ Html.text <| toString model ]
         , if model.paused then
             button [ onClick Resume ] [ Html.text "Resume" ]
           else
             button [ onClick Pause ] [ Html.text "Pause" ]
-        , div [] [ sceneView model ]
+        , div [] [ sceneView model.world ]
         ]
 
 
-sceneView model =
-    Svg.g
-        []
-        (List.map
-            entityView
-            model.entities
-        )
+sceneView world =
+    world.entities
+        |> Dict.values
+        |> List.map entityView
+        |> Svg.g []
         |> Svg.render2d
             (BoundingBox2d.with
                 { minX = -400
@@ -68,37 +196,28 @@ sceneView model =
             )
 
 
+entityView : Entity -> Svg Msg
 entityView entity =
     case entity of
         Bug bug ->
-            bugView bug
+            Svg.point2d
+                { radius = 3
+                , attributes =
+                    [ Svg.Attributes.stroke "red"
+                    , Svg.Attributes.fill "pink"
+                    ]
+                }
+                bug.position
 
         Food food ->
-            foodView food
-
-
-bugView : Bug -> Svg Msg
-bugView bug =
-    Svg.point2d
-        { radius = 3
-        , attributes =
-            [ Svg.Attributes.stroke "red"
-            , Svg.Attributes.fill "pink"
-            ]
-        }
-        bug.position
-
-
-foodView : Food -> Svg Msg
-foodView { position } =
-    Svg.point2d
-        { radius = 2
-        , attributes =
-            [ Svg.Attributes.stroke "green"
-            , Svg.Attributes.fill "lime"
-            ]
-        }
-        position
+            Svg.point2d
+                { radius = 2
+                , attributes =
+                    [ Svg.Attributes.stroke "green"
+                    , Svg.Attributes.fill "lime"
+                    ]
+                }
+                food.position
 
 
 type Msg
@@ -117,9 +236,7 @@ update msg model =
         Frame delta ->
             ( { model
                 | elapsed = model.elapsed + delta
-                , entities =
-                    model.entities
-                        |> List.map (updateEntity delta)
+                , world = world_update delta model.world
               }
             , Cmd.none
             )
@@ -131,23 +248,14 @@ update msg model =
             ( { model | paused = False }, Cmd.none )
 
 
-updateEntity delta entity =
-    case entity of
-        Bug bug ->
-            Bug (Bug.update delta bug)
-
-        Food food ->
-            Food (Food.update delta food)
-
-
 init =
     ( { elapsed = 0
       , paused = False
-      , entities =
-            [ Bug <| Bug.create ( 10, 10 )
-            , Bug <| Bug.create ( -10, 20 )
-            , Food <| Food.create ( 20, -20 )
-            ]
+      , world =
+            world_empty
+                |> world_insert (bug ( 10, 10 ))
+                |> world_insert (bug ( -10, 20 ))
+                |> world_insert (food ( 20, -20 ))
       }
     , Cmd.none
     )
